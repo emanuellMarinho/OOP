f9af3b001dc42448dd07c41d373f89ee6ba552e4
emanuelRM95 <joaoemanuelribeiromarinho@gmail.com>
2023-05-22 20:19:44 -0300

    Orientação a Objetos com Java

--- course2/src/application/Program.java
+++ course2/src/application/Program.java
@@ -21,7 +21,9 @@ public class Program {
 		System.out.print("Quantity in Stock: ");
 		product.quantity = sc.nextInt();
 		
-		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+		// name + ", " + product.price + ", " + product.quantity
+		
+		System.out.println(product);
 		
 		sc.close();
 	}
